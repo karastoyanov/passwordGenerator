b7f4c1e4d1817fd384cb9cb688796174a51da379
Aleksandar Karastoyanov <86146020+karastoyanov@users.noreply.github.com>
2022-03-04 00:05:28 +0200

    passGenerator

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
-using namespace std;
+#include <cstdlib>
 
+using namespace std;
+const char alphanum[] = "0123456789!@#$%^&*abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int string_length = sizeof(alphanum) - 1;
 int main()
 {
-
-    cout << "Hello" << endl;
-    cout << "Hello_2" << endl;
+    int n;
+    cout << "Enter the length of password:";
+    cin >> n;
+    cout << "Generated password: ";
+    for (int i = 0; i < n; i++)
+        cout << alphanum[rand() % string_length];
     return 0;
 }
